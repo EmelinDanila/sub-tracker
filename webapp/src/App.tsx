ee474600bd537852bf052004c2a72b62848ff627
Данила Емелин <danilaemelin@MacBook-Air-Danila.local>
2024-11-03 12:00:00 +0300

    Simpliest React component and backend setup

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
+  const subs = [
+    { id: '1', subscription: 'Subscription 1' },
+    { id: '2', subscription: 'Subscription 2' },
+    { id: '3', subscription: 'Subscription 3' },
+    { id: '4', subscription: 'Subscription 4' },
+    { id: '5', subscription: 'Subscription 5' },
+  ]
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Sub-tracker</h1>
+      <h2>My subscriptions</h2>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Subscription</th>
+            <th scope="col">Category</th>
+            <th scope="col">Price</th>
+            <th scope="col">Period</th>
+            <th scope="col">Write-off date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {subs.map((sub) => {
+            return (
+              <tr key={sub.id}>
+                <th scope="row">{sub.subscription}</th>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
   )
 }
-
-export default App
